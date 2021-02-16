4a93c7ca5bcb9914329fb43c3df0f070f67d9e6f
luk3Sky <ganindudananja@gmail.com>
2021-02-17 02:08:36 +0530

    Distance Sensor view improvements

--- src/js/sandbox_scripts/distanceSensor.js
+++ src/js/sandbox_scripts/distanceSensor.js
@@ -9,7 +9,7 @@ export function setup() {
   $(".btn").prop("disabled", false);
   $(".channel").text(window.mqtt.channel);
 
-  $("#robot-id")
+  $("#distance-sensor-robot-id")
     .change(function () {
       const robotId = this.value;
       const topic = `sensor/distance/${robotId}`;
@@ -24,11 +24,11 @@ export function setup() {
       });
 
       dist_robot_id = robotId;
-      $(".robot-id").text(robotId);
+      $(".distance-sensor-robot-id").text(robotId);
     })
     .change();
 
-  $("#dist-bar")
+  $("#distance-sensor-dist-bar")
     .change(function () {
       $("#dist-val").val(this.value);
       $(".dist-val").text(this.value);
@@ -42,20 +42,20 @@ export function setup() {
 
   // Publish: sensor/distance/${robotId}/?
   $("#dist-btn-req").click(function () {
-    const robotId = $("#robot-id").val();
+    const robotId = $("#distance-sensor-robot-id").val();
     mqtt.publish(`sensor/distance/${robotId}/?`, "?");
   });
 
   // Publish: sensor/distance/${robotId}
   $("#dist-btn-send-robot").click(function () {
-    const robotId = $("#robot-id").val();
+    const robotId = $("#distance-sensor-robot-id").val();
     const distance = $("#dist-val").val();
     mqtt.publish(`sensor/distance/${robotId}`, distance);
   });
 
   // Publish: sensor/distance/
   $("#dist-btn-send-server").click(function () {
-    const robotId = $("#robot-id").val();
+    const robotId = $("#distance-sensor-robot-id").val();
     const distance = $("#dist-val").val();
     const msgString = { id: robotId, dist: distance };
     mqtt.publish("sensor/distance", JSON.stringify(msgString));
