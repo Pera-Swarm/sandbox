1b8b3fcc0ee66cfad1729467d77a78165f27614b
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2021-02-20 22:13:16 +0530

    Update communication.js

--- js/communication.js
+++ js/communication.js
@@ -9,13 +9,14 @@ $(document).ready(function () {
         $('.btn').prop('disabled', false);
 
         // Subscribe: v1/comm/out/simple
-        mqtt.subscribeToTopic('comm/out/simple', (topic, msg) => {
-            $('#com-out-simple').text(msg);
+        mqtt.subscribeToTopic("comm/out/simple", (topic, msg) => {
+          $("#com-out-simple").text(msg);
         });
-        // Subscribe: v1/comm/out/directional
-        mqtt.subscribeToTopic('comm/out/directional', (topic, msg) => {
-            $('#com-out-directional').text(msg);
+        // Subscribe: v1/comm/out/direct
+        mqtt.subscribeToTopic("comm/out/direct", (topic, msg) => {
+          $("#com-out-direct").text(msg);
         });
+
 
         // Event listeners for select box updates
         $('#comm-out-protocol')
@@ -27,19 +28,28 @@ $(document).ready(function () {
         $('#robot-id')
             .change(function () {
                 const robotId = this.value;
-                const topic = `comm/in/${robotId}`;
+                const topicSimple = `comm/in/simple/${robotId}`;
+                const topicDirect = `comm/in/direct/${robotId}`;
 
                 if (comm_in_id != robotId && comm_in_id !== null) {
-                    // unsubscribe from previous topic
-                    mqtt.unsubscribe(`comm/in/${comm_in_id}`);
+                  // unsubscribe from previous topic
+                  mqtt.unsubscribe(`comm/in/simple/${comm_in_id}`);
                 }
-                mqtt.subscribeToTopic(topic, (topic, msg) => {
-                    $('#com-in-msg').text(msg);
-                    console.log(topic, ':', msg);
+
+                // Simple communication
+                mqtt.subscribeToTopic(topicSimple, (topicSimple, msg) => {
+                  $("#com-in-msg").text(`simple: ${msg}`);
+                  console.log(topicSimple, ":", msg);
+                });
+
+                // Directed communication
+                mqtt.subscribeToTopic(topicDirect, (topicDirect, msg) => {
+                  $("#com-in-msg").text(`simple: ${msg}`);
+                  console.log(topicDirect, ":", msg);
                 });
 
                 comm_in_id = robotId;
-                $('.robot-id').text(robotId);
+                $(".robot-id").text(robotId);
             })
             .change();
 
@@ -51,9 +61,10 @@ $(document).ready(function () {
 
         // Publish Button
         $('#comm-out-publish').click(function () {
-            const robotId = $('#robot-id').val();
-            const msg = $('#comm-out-msg').val();
-            mqtt.publish(`comm/in/${robotId}`, msg);
+            const robotId = $("#robot-id").val();
+            const msg = $("#comm-out-msg").val();
+            const msgProtocol = $("#comm-out-protocol").val();
+            mqtt.publish(`comm/in/${msgProtocol}/${robotId}`, msg);
         });
 
         // broadcast Button
