fb30698194244a7a350e7660b0381db81b5e6ea0
luk3Sky <ganindudananja@gmail.com>
2021-04-17 01:45:43 +0530

    Token support in store

--- src/js/store/cache.js
+++ src/js/store/cache.js
@@ -1,9 +1,13 @@
 import { createStore } from 'framework7';
+import axios from 'axios';
+import { saveConfig } from '../config';
+import config from '../config';
 
 const store = createStore({
     state: {
         loading: false,
-        logs: []
+        logs: [],
+        token: config.token
     },
     actions: {
         getLogs({ state }) {
@@ -24,6 +28,34 @@ const store = createStore({
                 );
                 state.loading = false;
             }, 1000);
+        },
+        createToken({ state }) {
+            state.loading = true;
+            axios
+                .post('http://webservices.ceykod.com/pera-swarm/login/', {
+                    user: window.username,
+                    pass: window.password,
+                    host: config.server,
+                    port: config.port,
+                    path: config.path
+                })
+                .then(
+                    (response) => {
+                        console.log(response);
+                        // TODO:
+                        // const token = response.data.token;
+                        // saveConfig(token);
+                        // localStorage.setItem(
+                        //     document.location.origin + `.cache`,
+                        //     JSON.stringify([])
+                        // );
+                        state.loading = false;
+                    },
+                    (error) => {
+                        console.log(error);
+                        state.loading = false;
+                    }
+                );
         }
     },
     getters: {
@@ -32,6 +64,9 @@ const store = createStore({
         },
         logs({ state }) {
             return state.logs;
+        },
+        token({ state }) {
+            return state.token;
         }
     }
 });
