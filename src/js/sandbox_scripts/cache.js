28917f304095a5ab160ec679f0a67ed63f981276
luk3Sky <ganindudananja@gmail.com>
2021-04-17 00:36:13 +0530

    Update cache.js

--- src/js/sandbox_scripts/cache.js
+++ src/js/sandbox_scripts/cache.js
@@ -3,7 +3,9 @@ import $ from 'jquery';
 export function setup() {
     console.log('Setup: Cache');
 
-    var cache_List = window.subList;
+    var cache_List = JSON.parse(
+        localStorage.getItem(document.location.origin + `.cache`)
+    );
     console.log(cache_List);
 
 }
