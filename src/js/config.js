f416eaafb31363d50b17f0d2b90d89ac77dfb8cf
Ganindu Dhananja <ganindudananja@gmail.com>
2021-08-28 12:32:49 +0530

    Config bug resolved

--- src/js/config.js
+++ src/js/config.js
@@ -48,7 +48,7 @@ export function getCredentials() {
 
     const { token } = resolvedConfig;
     console.log('getCredentials', resolvedConfig.token, token);
-    if (token !== undefined && token !== null) {
+    if (token !== undefined && token !== null && typeof token === 'string') {
         // decode the token
         return parseJwt(token);
     } else {
@@ -68,17 +68,19 @@ export function getCredentials() {
 // }
 
 function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(
-        atob(base64)
-            .split('')
-            .map(function (c) {
-                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-            })
-            .join('')
-    );
-    return JSON.parse(jsonPayload);
+    if (token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split('')
+                .map(function (c) {
+                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+                })
+                .join('')
+        );
+        return JSON.parse(jsonPayload);
+    }
 }
 
 export function saveCache(name, data) {
